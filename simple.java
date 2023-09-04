296f69cb315f3a75d1add335485bfafe4ca776b2
Siva1m <143991691+Siva1m@users.noreply.github.com>
2023-09-04 13:51:03 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -2,5 +2,8 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
         System.out.println("siva"); 
+        System.out.println("siva"); 
+        
+        
     }  
 }  
